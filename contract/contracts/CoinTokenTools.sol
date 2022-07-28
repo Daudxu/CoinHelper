5513d49ab3b3462ed99f3ef6627f1fe60249bf67
xudan <xudan@apowo.com>
2022-07-28 17:05:11 +0800

    fix update

--- contract/contracts/CoinTokenTools.sol
+++ contract/contracts/CoinTokenTools.sol
@@ -17,6 +17,21 @@ interface TokenErc {
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
+interface ERC721 {
+    event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
+    event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId);
+    event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
+    function balanceOf(address _owner) external view returns (uint256);
+    function ownerOf(uint256 _tokenId) external view returns (address);
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes data) external payable;
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId) external payable;
+    function transferFrom(address _from, address _to, uint256 _tokenId) external payable;
+    function approve(address _approved, uint256 _tokenId) external payable;
+    function setApprovalForAll(address _operator, bool _approved) external;
+    function getApproved(uint256 _tokenId) external view returns (address);
+    function isApprovedForAll(address _owner, address _operator) external view returns (bool);
+}
+
  contract CoinTokenTools is Initializable, OwnableUpgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -144,6 +159,11 @@ interface TokenErc {
              payable(_address[i]).transfer(_amount[i]);
         }
     }
+
+    /**
+     */
+
+
     
     
 }
